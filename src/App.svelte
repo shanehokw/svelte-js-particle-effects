e9860f32f7acadd4ea2a2a69d98e70ed47f54aec
shanehokw <kenwae@hotmail.com>
2022-10-21 01:06:38 +0800

    add option for users to upload own image

--- src/App.svelte
+++ src/App.svelte
@@ -23,183 +23,215 @@
 
 	let effect;
 	let handleSave;
+	var canvas;
+	var ctx;
 
-	window.addEventListener("load", function() {
-		const canvas = document.getElementById("canvas");
-		const ctx = canvas.getContext("2d");
-		const image = document.getElementById("image");
-		canvas.width = window.innerWidth;
-		canvas.height = window.innerHeight;
+	window.addEventListener("load", () => {
+		function drawOnCanvas(image){
+			canvas = document.getElementById("canvas");
+			ctx = canvas.getContext("2d", {willReadFrequently: true});
+			
+			canvas.width = window.innerWidth;
+			canvas.height = window.innerHeight;
+			
+			// step 1: draw image on canvas
+			ctx.drawImage(image, (canvas.width * 0.5) - (image.width * 0.5), (canvas.height * 0.5) - (image.height * 0.5));
+			// step 2: analyse canvas for pixel data
+			const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
 
-		// step 1: draw image on canvas
-		ctx.drawImage(image, (canvas.width * 0.5) - (image.width * 0.5), (canvas.height * 0.5) - (image.height * 0.5));
-		// step 2: analyse canvas for pixel data
-		const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
-
-		// each particle will be a pixel in the image
-		// break the image into individual pieces, at the same time
-		// these particles will react to mouse in a dynamic way with friction and physics
-		class Particle {
-			constructor(effect, x, y, color) {
-				this.effect = effect; // does not create a copy, just a reference to the memory of Effect class
-	
-				// determines initial position of particles
-				this.x = Math.random() * this.effect.width; // spread particles around canvas at initial page load
-				this.y = Math.random() * this.effect.height;
-	
-				// original coordinate of pixel before starting to move
-				// round down so anti-aliasing isnt needed, improves performance
-				this.originX = Math.floor(x);
-				this.originY = Math.floor(y);
-				this.color = color;
-	
-				this.size = this.effect.gap; // each particle rectangle will be size x size pixels
-	
-				// random value between -1 and +1
-				// this.velocityX = Math.random() * 2 - 1;
-				// this.velocityY = Math.random() * 2 - 1;
-	
-				this.velocityX = 0;
-				this.velocityY = 0;
-	
-				this.distanceX = 0;
-				this.distanceY = 0;
-				this.distance = 0;
-				this.force = 0;
-				this.angle = 0;
-				this.friction = friction; // higher value = less friction, particles get pushed further
-	
-				this.ease = ease; // controls speed particles return to origin
-			}
-	
-			draw(context) {
-				context.fillStyle = this.color;
-				context.fillRect(this.x, this.y, this.size, this.size);
-			}
-	
-			update() {
-				this.distanceX = this.effect.mouse.x - this.x;
-				this.distanceY = this.effect.mouse.y - this.y;
-	
-				// not using Math.sqrt for pythagoran distance because its an expensive operation,
-				// instead just set mouse radius to large value
-				this.distance =
-					this.distanceX * this.distanceX + this.distanceY * this.distanceY;
-	
-				// force pushing particles = mouse radius:distance of the particle to that mouse cursor
-				this.force = -this.effect.mouse.radius / this.distance;
-	
-				if (this.distance < this.effect.mouse.radius) {
-					this.angle = Math.atan2(this.distanceY, this.distanceX);
-					this.velocityX += this.force * Math.cos(this.angle);
-					this.velocityY += this.force * Math.sin(this.angle);
+			// each particle will be a pixel in the image
+			// break the image into individual pieces, at the same time
+			// these particles will react to mouse in a dynamic way with friction and physics
+			class Particle {
+				constructor(effect, x, y, color) {
+					this.effect = effect; // does not create a copy, just a reference to the memory of Effect class
+		
+					// determines initial position of particles
+					this.x = Math.random() * this.effect.width; // spread particles around canvas at initial page load
+					this.y = Math.random() * this.effect.height;
+		
+					// original coordinate of pixel before starting to move
+					// round down so anti-aliasing isnt needed, improves performance
+					this.originX = Math.floor(x);
+					this.originY = Math.floor(y);
+					this.color = color;
+		
+					this.size = this.effect.gap; // each particle rectangle will be size x size pixels
+		
+					// random value between -1 and +1
+					// this.velocityX = Math.random() * 2 - 1;
+					// this.velocityY = Math.random() * 2 - 1;
+		
+					this.velocityX = 0;
+					this.velocityY = 0;
+		
+					this.distanceX = 0;
+					this.distanceY = 0;
+					this.distance = 0;
+					this.force = 0;
+					this.angle = 0;
+					this.friction = friction; // higher value = less friction, particles get pushed further
+		
+					this.ease = ease; // controls speed particles return to origin
 				}
-	
-				// *= lets the particles return to original position
-				this.x +=
-					(this.velocityX *= this.friction) +
-					(this.originX - this.x) * this.ease;
-				this.y +=
-					(this.velocityY *= this.friction) +
-					(this.originY - this.y) * this.ease;
+		
+				draw(context) {
+					context.fillStyle = this.color;
+					context.fillRect(this.x, this.y, this.size, this.size);
+				}
+		
+				update() {
+					this.distanceX = this.effect.mouse.x - this.x;
+					this.distanceY = this.effect.mouse.y - this.y;
+		
+					// not using Math.sqrt for pythagoran distance because its an expensive operation,
+					// instead just set mouse radius to large value
+					this.distance =
+						this.distanceX * this.distanceX + this.distanceY * this.distanceY;
+		
+					// force pushing particles = mouse radius:distance of the particle to that mouse cursor
+					this.force = -this.effect.mouse.radius / this.distance;
+		
+					if (this.distance < this.effect.mouse.radius) {
+						this.angle = Math.atan2(this.distanceY, this.distanceX);
+						this.velocityX += this.force * Math.cos(this.angle);
+						this.velocityY += this.force * Math.sin(this.angle);
+					}
+		
+					// *= lets the particles return to original position
+					this.x +=
+						(this.velocityX *= this.friction) +
+						(this.originX - this.x) * this.ease;
+					this.y +=
+						(this.velocityY *= this.friction) +
+						(this.originY - this.y) * this.ease;
+				}
+		
+				warp() {
+					this.x = Math.random() * this.effect.width;
+					this.y = Math.random() * this.effect.height;
+					// this.ease = Math.random() * (0.05 - 0.01) + 0.01; // random number between 0.01 to 0.05
+				}
 			}
-	
-			warp() {
-				this.x = Math.random() * this.effect.width;
-				this.y = Math.random() * this.effect.height;
-				// this.ease = Math.random() * (0.05 - 0.01) + 0.01; // random number between 0.01 to 0.05
-			}
-		}
 
-		class Effect {
-			constructor(width, height) {
-				this.width = width;
-				this.height = height;
-				this.particlesArray = [];
-				// this.image = image;
-				this.centerX = this.width * 0.5;
-				this.centerY = this.height * 0.5;
-				this.x = this.centerX - image.width * 0.5;
-				this.y = this.centerY - image.height * 0.5;
-				this.gap = pixelSize; // higher value = better performance but bigger paricle chunk
-				this.mouse = {
-					radius: mouseRadius,
-					x: undefined,
-					y: undefined,
-				};
-				
-				canvas.addEventListener("touchmove", (event) => {
-					this.mouse.x = event.touches[0].clientX;
-					this.mouse.y = event.touches[0].clientY;
-				});
+			class Effect {
+				constructor(width, height) {
+					this.width = width;
+					this.height = height;
+					this.particlesArray = [];
+					// this.image = image;
+					this.centerX = this.width * 0.5;
+					this.centerY = this.height * 0.5;
+					this.x = this.centerX - image.width * 0.5;
+					this.y = this.centerY - image.height * 0.5;
+					this.gap = pixelSize; // higher value = better performance but bigger paricle chunk
+					this.mouse = {
+						radius: mouseRadius,
+						x: undefined,
+						y: undefined,
+					};
+					
+					canvas.addEventListener("touchmove", (event) => {
+						this.mouse.x = event.touches[0].clientX;
+						this.mouse.y = event.touches[0].clientY;
+					});
 
-				canvas.addEventListener("mousemove", (event) => {
-					this.mouse.x = event.x;
-					this.mouse.y = event.y;
-				});
-	
-				// window.addEventListener("mouseup", (event) => {
-				// 	this.holding = false;
-				// 	this.mouse.x = event.x;
-				// 	this.mouse.y = event.y;
-				// });
-			}
-			init() {
-				// step 3: loop through pixels
-				for (let y = 0; y < this.height; y += this.gap) {
-					for (let x = 0; x < this.width; x += this.gap) {
-						// x4 because each pixel is represented by 4 consecutive values in the unsigned 8 bit clamped array
-						const index = (y * this.width + x) * 4;
-						const red = pixels[index];
-						const green = pixels[index + 1];
-						const blue = pixels[index + 2];
-						const alpha = pixels[index + 3];
-						const color = `rgb(${red}, ${green}, ${blue})`;
-	
-						// only if pixel is not transparent
-						if (alpha > 0) {
-							this.particlesArray.push(new Particle(this, x, y, color));
+					canvas.addEventListener("mousemove", (event) => {
+						this.mouse.x = event.x;
+						this.mouse.y = event.y;
+					});
+				}
+				init() {
+					// step 3: loop through pixels
+					for (let y = 0; y < this.height; y += this.gap) {
+						for (let x = 0; x < this.width; x += this.gap) {
+							// x4 because each pixel is represented by 4 consecutive values in the unsigned 8 bit clamped array
+							const index = (y * this.width + x) * 4;
+							const red = pixels[index];
+							const green = pixels[index + 1];
+							const blue = pixels[index + 2];
+							const alpha = pixels[index + 3];
+							const color = `rgb(${red}, ${green}, ${blue})`;
+		
+							// only if pixel is not transparent
+							if (alpha > 0) {
+								this.particlesArray.push(new Particle(this, x, y, color));
+							}
 						}
 					}
 				}
-			}
-	
-			draw(context) {
-				this.particlesArray.forEach((particle) => particle.draw(context));
-			}
-	
-			update() {
-				this.particlesArray.forEach((particle) => particle.update());
-			}
-	
-			warp() {
-				this.particlesArray.forEach((particle) => particle.warp());
-			}
-		}
-	
-		function createEffect() {
-			if (effect) {
-				effect = null;
-			}
-			effect = new Effect(canvas.width, canvas.height);
-			effect.init();
-		}
-	
-		function animate() {
-			ctx.clearRect(0, 0, canvas.width, canvas.height);
-			effect.draw(ctx);
-			effect.update();
-			requestAnimationFrame(animate);
-		}
-	
-		createEffect();
-		animate();
 		
-		handleSave = () => {
+				draw(context) {
+					this.particlesArray.forEach((particle) => particle.draw(context));
+				}
+		
+				update() {
+					this.particlesArray.forEach((particle) => particle.update());
+				}
+		
+				warp() {
+					this.particlesArray.forEach((particle) => particle.warp());
+				}
+			}
+		
+			function createEffect() {
+				if (effect) {
+					effect = null;
+				}
+				effect = new Effect(canvas.width, canvas.height);
+				effect.init();
+			}
+		
+			function animate() {
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				effect.draw(ctx);
+				effect.update();
+				requestAnimationFrame(animate);
+			}
+		
 			createEffect();
-			toggle();
+			animate();
+			
+			handleSave = () => {
+				createEffect();
+				toggle();
+			}
 		}
-	})
+
+		
+		const imageInput = document.getElementById("image-input");
+		
+		imageInput.addEventListener("change", () => {
+			// var imageFile = imageInput.files;
+			// if (imageFile.length > 0) {
+			// 	var imageSrc = imageInput[0];
+				
+			// 	const fileReader = new FileReader();
+			// 	fileReader.addEventListener("load", () => {
+			// 		console.log(fileReader.result);
+			// 	});
+			// 	var image64 = fileReader.readAsDataURL(imageSrc);
+			// 	console.log(image64);
+			// }
+			const img = document.getElementById("image");
+			const file = imageInput.files[0];
+			const reader = new FileReader();
+			reader.addEventListener("load", () => {
+				img.src = reader.result;
+				ctx.clearRect(0, 0, canvas.width, canvas.height);
+				img.addEventListener("load", () => {
+					drawOnCanvas(img);
+				});
+			})
+			if (file) {
+				var fileData = reader.readAsDataURL(file);
+			}
+		});
+
+		const image = document.getElementById("image");
+		drawOnCanvas(image);
+	});
+
 </script>
 
 <svelte:head>
@@ -209,6 +241,23 @@
 </svelte:head>
 <main>
 	<canvas id="canvas"></canvas>
+
+	<div class="image-selection">
+		<label for="image-input" class="image-options" aria-label="Choose image to upload">
+			<Icon name="plus" style="font-size: 2rem;"></Icon>
+		</label>
+		<input id="image-input" type="file" accept="image/*"/>
+
+		<!-- <label for="image-input2" class="image-options" aria-label="Choose image to upload">
+			<Icon name="plus" style="font-size: 2rem;"></Icon>
+		</label>
+		<input id="image-input2" type="file" accept="image/*"/>
+
+		<label for="image-input3" class="image-options" aria-label="Choose image to upload">
+			<Icon name="plus" style="font-size: 2rem;"></Icon>
+		</label>
+		<input id="image-input3" type="file" accept="image/*"/> -->
+	</div>
 
 	<img
 		id="image" 
@@ -224,10 +273,6 @@
 			<Icon name="controller" style="font-size: 2rem;" />
 		</Button>
 	</div>
-
-	<!-- <Fab color="primary" on:click={toggle}>
-		<Icon class="material-icons">favorite</Icon>
-	</Fab> -->
 </main>
 
 <Modal isOpen={open} {toggle}>
@@ -269,6 +314,46 @@
 		overflow: hidden;
 		font-family: Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI", 
         Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif !important;
+	}
+
+	.image-selection {
+		display: flex;
+		justify-content: center;
+		padding-top: 2.5rem;
+		position: absolute;
+		width: 100%;
+		gap: 1rem;
+	}
+
+	.image-selection > * {
+		/* position: absolute;
+		z-index: 1000; */
+	}
+
+	.image-selection > input[type=file] {
+		opacity: 0;
+
+		/* hide the input itself as it is hard to style */
+		/* apply styles to <label> because clicking on label works the same as clicking on input */
+		width: 0;
+		height: 0;
+	}
+
+	.image-options {
+		border: 0.25rem solid white;
+		width: 4rem;
+		height: 4rem;
+		color: white;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border-radius: 10%;
+	}
+
+	.image-options:hover {
+		cursor: pointer;
+		background: white;
+		color: black;
 	}
 
 	#canvas {
